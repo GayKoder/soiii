52a583335fa2250ace58b3dc4732d942791b2219
Timur Gainutdinov <timurga2001@gmail.com>
2024-04-03 00:53:42 +0300

    modified:   src/components/AboutProject.tsx

--- src/components/AboutProject.tsx
+++ src/components/AboutProject.tsx
@@ -1,4 +1,4 @@
-import React from "react";
+
 import { AddressInfo, TokenInfoItem } from "../App";
 
 type InfoItem = {
--- vite.config.ts
+++ vite.config.ts
@@ -2,7 +2,7 @@ import { defineConfig, loadEnv } from 'vite'
 import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
-export default defineConfig((mode ) => {
+export default defineConfig((mode) => {
   const env = loadEnv(mode, process.cwd(), '');
   return {
     define: {
@@ -11,5 +11,15 @@ export default defineConfig((mode ) => {
       'process.env.RECIEVER_ADDRESS': JSON.stringify(env.RECIEVER_ADDRESS)
     },
     plugins: [react()],
+    preview: {
+      port: 3000,
+      strictPort: true,
+     },
+     server: {
+      port: 3000,
+      strictPort: true,
+      host: true,
+      origin: "http://0.0.0.0:3000",
+     },
   }
 })
